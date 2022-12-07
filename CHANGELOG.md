bf21d2c49694770ae660992cd3cc4c19d4564623
Tlaloc-Es <jfuentes@jokalabs.com>
2022-12-07 21:55:52 +0100

    bump: version 0.0.1 → 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.1.0 (2022-12-07)
+
+### Feat
+
+- **building-first-converter-version**: add support to convert file from labelme segmentation to yolov7
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "Readme.md").read_text()
 
 setup(
     name="labelme2yolov7segmentation",
-    version="0.0.1",
+    version="0.1.0",
     description="Convert LabelMe format to yolov7 for segmentation.",
     long_description=README,
     long_description_content_type="text/markdown",
